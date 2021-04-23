1af98b64bb14f5c9fbe4e91d5c0b19486ec1e3c9
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-04-23 22:04:21 +0530

    elective / forms page

--- src/app/page-electives/page-electives.module.ts
+++ src/app/page-electives/page-electives.module.ts
@@ -3,21 +3,39 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PageElectivesComponent } from './page-electives.component';
+import { CardModule } from "primeng/card";
+import { TabViewModule } from "primeng/tabview";
+import { InputTextModule } from "primeng/inputtext";
+import { CalendarModule } from "primeng/calendar";
+import { FormsModule } from "@angular/forms";
+import { DropdownModule } from "primeng/dropdown";
+import { AutoCompleteModule } from "primeng/autocomplete";
+import { ButtonModule } from "primeng/button";
+import { ChipModule } from "primeng/chip";
 
 const PageElectivesRoutes: Routes = [
-  {
-    path: '',
-    component: PageElectivesComponent
-  }
+    {
+        path: '',
+        component: PageElectivesComponent
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(PageElectivesRoutes)
-  ],
-  declarations: [
-    PageElectivesComponent
-  ]
+    imports: [
+        CommonModule,
+        RouterModule.forChild(PageElectivesRoutes),
+        CardModule,
+        TabViewModule,
+        InputTextModule,
+        CalendarModule,
+        FormsModule,
+        DropdownModule,
+        AutoCompleteModule,
+        ButtonModule,
+        ChipModule
+    ],
+    declarations: [
+        PageElectivesComponent
+    ]
 })
 export class PageElectivesModule { }
