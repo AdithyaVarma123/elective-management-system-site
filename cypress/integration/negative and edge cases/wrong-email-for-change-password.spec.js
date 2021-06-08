3e18de129e43b31b339fd4fa891eb1942ede2c48
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-06-08 12:15:08 +0530

    Update wrong-email-for-change-password.spec.js

--- cypress/integration/negative and edge cases/wrong-email-for-change-password.spec.js
+++ cypress/integration/negative and edge cases/wrong-email-for-change-password.spec.js
@@ -1,15 +1,19 @@
 // eslint-disable-next-line no-undef
 describe("Login", () => {
     it("wrong email for reset password", () => {
+        cy.intercept('PUT', 'https://amrita-elective.tk/users/requestReset',
+            {
+                statusCode: 200,
+                body: {
+                  status: true,
+                  message: 'Reset email sent!'
+                }
+            }
+        ).as('/users/requestReset');
         cy.visit("http://localhost:4200/");
         cy.get('.p-button-danger > .p-button-label').click();
-        cy.get('#emailId').type('adithyapv2k@gmail.com');
+        cy.get('#emailId').type('kjosephsubash@gmail.com');
         cy.get('.ng-star-inserted > .p-button > .p-button-label').click();
         cy.get('.p-dialog-header-close-icon').click();
-        cy.intercept('https://amrita-elective.tk/users/resetPassword',(req) => {
-            req.body = {
-                "oldPassword": "admin",
-                "newPassword": "admin"
-            };});
     });
 });
