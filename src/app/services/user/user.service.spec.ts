e497e01b19efec6d8a41b010b459088aa0806e9e
Joseph Subash Kanichai <kjosephsubash@gmail.com>
2021-06-10 12:26:12 +0530

    Update user.service.spec.ts

--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -2,13 +2,15 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { MessageService } from "primeng/api";
 
 describe('UserService', () => {
     let service: UserService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HttpClientModule]
+            imports: [HttpClientModule],
+            providers: [MessageService]
         });
         service = TestBed.inject(UserService);
     });
